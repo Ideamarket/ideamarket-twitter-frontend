25260b6daf03bc9abbbd7ebff4e2f8ec5bf92187
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2020-10-20 14:50:21 +0200

    Add initial list token action

---
+++ src/actions/listToken.ts
@@ -0,0 +1,6 @@
+import { useContractStore } from '../store/contractStore'
+
+export async function listToken(name: string, marketID: number) {
+  const factoryContract = useContractStore.getState().factoryContract
+  return factoryContract.methods.addToken(name, marketID.toString()).send()
+}
