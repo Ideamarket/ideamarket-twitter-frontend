7ed5bd0bf6ecde51dabdccd8f3e31ec9a686c038
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2020-11-03 11:29:32 +0100

    Use UseEffect in erc20Balance

--- src/actions/erc20Balance.ts
+++ src/actions/erc20Balance.ts
@@ -11,28 +11,30 @@ export function useERC20Balance(address: string) {
   const [balanceBN, setBalanceBN] = useState(undefined)
   const [balance, setBalance] = useState(undefined)
 
-  if (useWalletStore.getState().web3 && address) {
-    const contract = getERC20Contract(address)
-    contract.methods
-      .balanceOf(useWalletStore.getState().address)
-      .call()
-      .then((data, error) => {
-        if (error) {
-          console.log(error)
-          setBalanceBN(new BN('0'))
-          setBalance('0.00')
-          setIsLoading(false)
-          return
-        }
+  useEffect(() => {
+    if (useWalletStore.getState().web3 && address) {
+      const contract = getERC20Contract(address)
+      contract.methods
+        .balanceOf(useWalletStore.getState().address)
+        .call()
+        .then((data, error) => {
+          if (error) {
+            console.log(error)
+            setBalanceBN(new BN('0'))
+            setBalance('0.00')
+            setIsLoading(false)
+            return
+          }
 
-        setBalanceBN(data)
-        setBalance(web3BNToFloatString(data, tenPow18, 2))
-        setIsLoading(false)
-      })
-      .catch((error) => {
-        console.log(error)
-      })
-  }
+          setBalanceBN(data)
+          setBalance(web3BNToFloatString(data, tenPow18, 2))
+          setIsLoading(false)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+  })
 
   return [isLoading, balanceBN, balance]
 }
