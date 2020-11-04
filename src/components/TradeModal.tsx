8de89cc4713dfc8e09922301b49dedddc704b5f5
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2020-11-04 13:48:20 +0100

    Only allow positive number for amount input

--- src/components/TradeModal.tsx
+++ src/components/TradeModal.tsx
@@ -243,6 +243,7 @@ export default function TradeModal({
           <input
             className="w-full px-4 py-2 leading-tight bg-gray-200 border-2 border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-brand-blue"
             type="number"
+            min="0"
             onChange={(event) => {
               setIdeaTokenAmount(event.target.value)
             }}
