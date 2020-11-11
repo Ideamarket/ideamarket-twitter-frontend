900f66d3877065ad7914c52a6fca0556e1ad0844
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2020-11-11 10:54:16 +0100

    Add 2-col Chart section

--- src/pages/details/[address].tsx
+++ src/pages/details/[address].tsx
@@ -119,9 +119,34 @@ export default function TokenDetails() {
     )
   }
 
-  function makeContainerWithHeader(header, content) {
+  function makeDetailsSkeleton() {
     return (
-      <div className="flex flex-col overflow-hidden rounded-md bg-brand-gray">
+      <div className="mx-auto bg-gray-400 rounded w-15 animate animate-pulse">
+        <span className="invisible">A</span>
+      </div>
+    )
+  }
+
+  function makeDetailsOverChartEntry(header, content, withBorder) {
+    return (
+      <div
+        className="text-center rounded-sm"
+        style={{ borderRight: withBorder && '1px solid #cbd5e0' }}
+      >
+        <div className="text-xs text-brand-gray-2">{header}</div>
+        <div className="text-2xl">{content}</div>
+      </div>
+    )
+  }
+
+  function makeContainerWithHeader(header, content, spanCols?) {
+    return (
+      <div
+        className={classNames(
+          'flex flex-col overflow-hidden rounded-md bg-brand-gray',
+          spanCols ? `col-span-${spanCols}` : ''
+        )}
+      >
         <div
           className="flex items-center flex-grow-0 py-1 pl-2.5 text-sm border-gray-400 bg-very-dark-blue text-brand-gray"
           style={{ borderBottomWidth: '1px' }}
@@ -129,14 +154,6 @@ export default function TokenDetails() {
           {header}
         </div>
         <div className="flex-grow p-1">{content}</div>
-      </div>
-    )
-  }
-
-  function makeDetailsSkeleton() {
-    return (
-      <div className="mx-auto bg-gray-400 rounded w-15 animate animate-pulse">
-        <span className="invisible">A</span>
       </div>
     )
   }
@@ -209,118 +226,76 @@ export default function TokenDetails() {
 
         <div className="grid grid-cols-1 gap-5 mx-5 mt-5 text-black md:grid-cols-2">
           {makeContainerWithHeader(
-            'Details',
-            <div className="grid h-full grid-cols-3 gap-7">
-              {makeDetailsEntry(
-                'Price',
-                isLoading
-                  ? makeDetailsSkeleton()
-                  : '$' +
-                      web3BNToFloatString(
-                        calculateCurrentPriceBN(
-                          token.rawSupply,
-                          market.rawBaseCost,
-                          market.rawPriceRise
-                        ),
-                        tenPow18,
-                        2
-                      )
-              )}
-
-              {makeDetailsEntry(
-                'Deposits',
-                isLoading ? (
-                  makeDetailsSkeleton()
-                ) : parseFloat(token.daiInToken) <= 0.0 ? (
-                  <>&mdash;</>
-                ) : (
-                  `$${token.daiInToken}`
-                )
-              )}
-
-              {makeDetailsEntry(
-                'Supply',
-                isLoading ? (
-                  makeDetailsSkeleton()
-                ) : parseFloat(token.supply) <= 0.0 ? (
-                  <>&mdash;</>
-                ) : (
-                  `${token.supply}`
-                )
-              )}
-
-              {makeDetailsEntry(
-                '24H Change',
-                isLoading ? (
-                  makeDetailsSkeleton()
-                ) : (
-                  <div
-                    className={
-                      parseFloat(token.dayChange) >= 0.0
-                        ? 'text-brand-green'
-                        : 'text-brand-red'
-                    }
-                  >
-                    {token.dayChange}%
-                  </div>
-                )
-              )}
-
-              {makeDetailsEntry(
-                '24H Volume',
-                isLoading ? makeDetailsSkeleton() : `$${token.dayVolume}`
-              )}
-
-              {makeDetailsEntry(
-                '1YR Income',
-                isLoading
-                  ? makeDetailsSkeleton()
-                  : `$${(
-                      parseFloat(token.daiInToken) * compoundSupplyRate
-                    ).toFixed(2)}`
-              )}
-
-              {makeDetailsEntry(
-                'Listed at',
-                isLoading
-                  ? makeDetailsSkeleton()
-                  : moment(token.listedAt * 1000).format('MMM Do YYYY')
-              )}
-
-              {makeDetailsEntry(
-                'Holders',
-                isLoading ? makeDetailsSkeleton() : token.holders
-              )}
-
-              {makeDetailsEntry(
-                'Watch',
-                isLoading ? (
-                  makeDetailsSkeleton()
-                ) : (
-                  <div className="flex justify-center mt-1">
-                    <WatchingStar token={token} />
-                  </div>
-                )
-              )}
-            </div>
-          )}
-
-          {makeContainerWithHeader(
             'Chart',
-            isLoading ? (
-              <div
-                className="w-full mx-auto bg-gray-400 rounded animate animate-pulse"
-                style={{
-                  minHeight: '190px',
-                  marginTop: '5px',
-                  marginBottom: '5px',
-                }}
-              ></div>
-            ) : (
-              <div style={{ minHeight: '200px' }}>
-                <PriceChart chartData={chartData} />
+            <>
+              <div className="grid grid-cols-4">
+                {makeDetailsOverChartEntry(
+                  'Price',
+                  isLoading
+                    ? makeDetailsSkeleton()
+                    : '$' +
+                        web3BNToFloatString(
+                          calculateCurrentPriceBN(
+                            token.rawSupply,
+                            market.rawBaseCost,
+                            market.rawPriceRise
+                          ),
+                          tenPow18,
+                          2
+                        ),
+                  true
+                )}
+
+                {makeDetailsOverChartEntry(
+                  'Deposits',
+                  isLoading ? (
+                    makeDetailsSkeleton()
+                  ) : parseFloat(token.daiInToken) <= 0.0 ? (
+                    <>&mdash;</>
+                  ) : (
+                    `$${token.daiInToken}`
+                  ),
+                  true
+                )}
+
+                {makeDetailsOverChartEntry(
+                  'Supply',
+                  isLoading ? (
+                    makeDetailsSkeleton()
+                  ) : parseFloat(token.supply) <= 0.0 ? (
+                    <>&mdash;</>
+                  ) : (
+                    `${token.supply}`
+                  ),
+                  true
+                )}
+
+                {makeDetailsOverChartEntry(
+                  'Holders',
+                  isLoading ? makeDetailsSkeleton() : token.holders,
+                  false
+                )}
               </div>
-            )
+              <div
+                className="my-1"
+                style={{ borderBottom: '1px solid #cbd5e0' }}
+              ></div>
+              <div style={{ minHeight: '200px' }}>
+                {isLoading ? (
+                  <div
+                    className="w-full mx-auto bg-gray-400 rounded animate animate-pulse"
+                    style={{
+                      minHeight: '190px',
+                      marginTop: '5px',
+                      marginBottom: '5px',
+                    }}
+                  ></div>
+                ) : (
+                  <PriceChart chartData={chartData} />
+                )}
+              </div>
+            </>,
+            2
           )}
 
           {makeContainerWithHeader(
