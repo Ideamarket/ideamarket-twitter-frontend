79d8fa437e7385c50fd458c3a763c3138080aef3
Bhanu Teja P <pbteja1998@gmail.com>
2020-11-17 11:54:18 +0530

    Move the modal close button outside of modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -50,22 +50,24 @@ export default function Modal({
             leave="ease-in duration-200"
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            className="inline-block overflow-hidden text-left transition-all transform bg-white rounded-lg shadow-xl w-76"
+            className="inline-block overflow-hidden text-left transition-all transform rounded-lg shadow-xl w-76"
             role="dialog"
             aria-modal="true"
             aria-labelledby="modal-headline"
           >
-            <div className="absolute top-0 right-0 pr-1 md:pr-4 md:pt-4">
+            <div className="flex pb-1">
               <button
                 type="button"
-                className="text-gray-400 transition duration-150 ease-in-out hover:text-gray-500 focus:outline-none focus:text-gray-500"
+                className="ml-auto text-white transition duration-150 ease-in-out hover:text-gray-500 focus:outline-none focus:text-gray-500"
                 aria-label="Close"
                 onClick={() => close()}
               >
                 <Close className="w-6 h-6" />
               </button>
             </div>
-            <div>{children}</div>
+            <div className="pb-1 overflow-hidden bg-white rounded-lg">
+              {children}
+            </div>
           </Transition>
         </div>
       </div>
