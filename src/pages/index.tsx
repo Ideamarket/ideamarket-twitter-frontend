c74913d34c1b8d1bde0880d4eff08991da6813e6
Bhanu Teja P <pbteja1998@gmail.com>
2020-11-17 11:35:15 +0530

    Move to corresponding help sections from footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -219,17 +219,17 @@ export default function Home() {
 
         <div className="md:flex">
           <div className="flex justify-center mt-10 space-x-5 font-sf-pro-text">
-            <Link href="/help">
+            <Link href="/help#need-help">
               <a className="text-sm leading-none tracking-tightest-2 text-brand-gray-2">
                 Need Help?
               </a>
             </Link>
-            <Link href="/help">
+            <Link href="/help#contact">
               <a className="text-sm leading-none tracking-tightest-2 text-brand-gray-2">
                 Contact
               </a>
             </Link>
-            <Link href="/help">
+            <Link href="/help#legal-and-privacy">
               <a className="text-sm leading-none tracking-tightest-2 text-brand-gray-2">
                 Legal &amp; Privacy
               </a>
