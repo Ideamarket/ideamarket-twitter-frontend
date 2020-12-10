8e7914874c39285f761c73fc4a0451475679bd08
Chaitanya S <yourchaitu@gmail.com>
2020-12-10 18:59:19 +0530

    Fix

--- src/pages/details/[marketName]/[tokenName].tsx
+++ src/pages/details/[marketName]/[tokenName].tsx
@@ -420,7 +420,7 @@ export default function TokenDetails() {
               content={
                 <div className="flex flex-col h-full pb-2">
                   <div
-                    className="grid w-full md:grid-cols-3 sm:grid-cols-2 p-5 border-gray-400 gap-7"
+                    className="grid w-full md:grid-cols-3 grid-cols-2 p-5 border-gray-400 gap-7"
                     style={{ borderBottomWidth: '1px' }}
                   >
                     <DetailsEntry
