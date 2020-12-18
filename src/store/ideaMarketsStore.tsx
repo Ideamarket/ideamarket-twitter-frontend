eae534d55d7a3274edc47908dff0f3a97dbe88eb
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2020-12-18 18:43:27 +0100

    Use our own Subgraph again

--- src/store/ideaMarketsStore.tsx
+++ src/store/ideaMarketsStore.tsx
@@ -10,7 +10,7 @@ const tenPow18 = new BigNumber('10').pow(new BigNumber('18'))
 
 const HTTP_GRAPHQL_ENDPOINT =
   NETWORK === 'rinkeby'
-    ? 'https://api.thegraph.com/subgraphs/name/ideamarket/ideamarketrinkeby' //'https://subgraph-rinkeby.backend.ideamarket.io:8080/subgraphs/name/Ideamarket/IdeamarketRINKEBY'
+    ? 'https://subgraph-rinkeby.backend.ideamarket.io:8080/subgraphs/name/Ideamarket/IdeamarketRINKEBY'
     : NETWORK === 'test'
     ? 'https://subgraph-test.backend.ideamarket.io:8080/subgraphs/name/Ideamarket/IdeamarketTEST'
     : 'https://api.thegraph.com/subgraphs/name/ideamarket/ideamarket'
