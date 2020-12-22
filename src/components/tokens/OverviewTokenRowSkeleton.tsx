4254f479e5cd04649d8e556f27ee8d45c473848b
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2020-12-22 14:15:21 +0100

    Remove OverviewTokenRowSkeleton scroll bar

--- src/components/tokens/OverviewTokenRowSkeleton.tsx
+++ src/components/tokens/OverviewTokenRowSkeleton.tsx
@@ -23,23 +23,23 @@ export default function TokenRowSkeleton({
             </div>
           </div>
         </td>
-        <td className="px-6 py-4 whitespace-nowrap">
+        <td className="px-5 py-4 whitespace-nowrap">
           <p className="w-10 h-4 bg-gray-400 rounded md:hidden"></p>
           <p className="h-4 mt-1 bg-gray-400 rounded"></p>
         </td>
-        <td className="px-6 py-4 whitespace-nowrap">
+        <td className="px-5 py-4 whitespace-nowrap">
           <p className="w-10 h-4 bg-gray-400 rounded md:hidden"></p>
           <p className="h-4 mt-1 bg-gray-400 rounded"></p>
         </td>
-        <td className="px-6 py-4 whitespace-nowrap">
+        <td className="px-5 py-4 whitespace-nowrap">
           <p className="w-10 h-4 bg-gray-400 rounded md:hidden"></p>
           <p className="h-4 mt-1 bg-gray-400 rounded"></p>
         </td>
-        <td className="px-6 py-4 whitespace-nowrap">
+        <td className="px-5 py-4 whitespace-nowrap">
           <p className="w-10 h-4 bg-gray-400 rounded md:hidden"></p>
           <p className="h-4 mt-1 bg-gray-400 rounded"></p>
         </td>
-        <td className="px-6 py-4 whitespace-nowrap">
+        <td className="px-5 py-4 whitespace-nowrap">
           <p className="w-10 h-4 bg-gray-400 rounded md:hidden"></p>
           <p className="h-4 mt-1 bg-gray-400 rounded"></p>
         </td>
@@ -47,17 +47,17 @@ export default function TokenRowSkeleton({
         <td className="col-span-3 row-span-1 row-start-4 px-6 py-4 text-sm leading-5 text-gray-500 whitespace-nowrap col-start-0">
           <p className="h-4 mt-1 bg-gray-400 rounded"></p>
         </td>
-        <td className="hidden px-6 py-4 whitespace-nowrap md:table-cell">
+        <td className="hidden px-5 py-4 whitespace-nowrap md:table-cell">
           <button className="h-10 bg-gray-400 rounded-lg w-28">
             <span className="invisible">Trade</span>
           </button>
         </td>
-        <td className="hidden px-6 py-4 whitespace-nowrap md:table-cell">
+        <td className="hidden px-5 py-4 whitespace-nowrap md:table-cell">
           <button className="h-10 bg-gray-400 rounded-lg w-28">
             <span className="invisible">Trade</span>
           </button>
         </td>
-        <td className="px-6 py-4 whitespace-nowrap">
+        <td className="px-3 py-4 whitespace-nowrap">
           <div className="w-5 h-5 bg-gray-400 rounded"></div>
         </td>
       </tr>
