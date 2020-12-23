3ab66c7f32575d4567bfdfac1075fa82f925ca6a
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2020-12-23 12:28:50 +0100

    Add Google Analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,11 +12,47 @@ class MyDocument extends Document {
     return { ...initialProps }
   }
 
+  static GA_TRACKING_ID = process.browser
+    ? process.env.NEXT_PUBLIC_NETWORK === 'mainnet'
+      ? 'UA-185970351-2'
+      : 'UA-185970351-2'
+    : ''
+
+  static pageview = (url) => {
+    ;(window as any).gtag('config', MyDocument.GA_TRACKING_ID, {
+      page_path: url,
+    })
+  }
+
+  static event = ({ action, category, label, value }) => {
+    ;(window as any).gtag('event', action, {
+      event_category: category,
+      event_label: label,
+      value: value,
+    })
+  }
+
   render() {
     return (
       <Html lang="en">
         <Head>
           <link rel="shortcut icon" href="/logo.png" />
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${MyDocument.GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${MyDocument.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </Head>
         <body>
           <Main />
