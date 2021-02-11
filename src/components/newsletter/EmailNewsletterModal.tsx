5b6011f51659e7ab792f3808a9e2fbc030a882e4
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2021-02-11 12:22:28 +0100

    Remove all text from Notifications popup

--- src/components/newsletter/EmailNewsletterModal.tsx
+++ src/components/newsletter/EmailNewsletterModal.tsx
@@ -18,15 +18,9 @@ export default function EmailNewsletterModal() {
       <div className="mb-5 w-100 max-w-100">
         <div className="p-4 bg-top-mobile">
           <p className="text-2xl text-center text-gray-300 md:text-3xl font-gilroy-bold">
-            New Markets
+            Notifications
           </p>
         </div>
-        <p className="m-5 break-normal">
-          If you missed the mad rush to list your favorite Twitter accounts,
-          don't worry. More <strong>new markets are on the way</strong>.<br />
-          <br />
-          Sign up to be notified 24 hours before they launch:
-        </p>
         <div className="flex justify-center">
           <iframe
             src="https://ideamarkets.substack.com/embed"
