cc033d947d3f702bf6efb768a8de7cd92ec2e52c
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2021-02-12 12:52:30 +0100

    Locked table fixes

--- src/components/tokens/LockedTokenRow.tsx
+++ src/components/tokens/LockedTokenRow.tsx
@@ -2,27 +2,25 @@ import moment from 'moment'
 
 import { LockedAmount } from 'store/ideaMarketsStore'
 
-export default function MyTokenRow({
+export default function LockedTokenRow({
   lockedAmount,
 }: {
   lockedAmount: LockedAmount
 }) {
   return (
-    <>
-      <tr className="table-row cursor-pointer bg-brand-gray hover:bg-white">
-        <td className="px-6 py-3 whitespace-nowrap">
-          <div className="flex items-center">
-            <div className="w-auto h-auto">
-              {moment(lockedAmount.lockedUntil * 1000).format('LLL')}
-            </div>
+    <div className="table-row cursor-pointer bg-brand-gray hover:bg-white">
+      <div className="px-6 py-3 whitespace-nowrap">
+        <div className="flex items-center">
+          <div className="w-auto h-auto">
+            {moment(lockedAmount.lockedUntil * 1000).format('LLL')}
           </div>
-        </td>
-        <td className="px-6 py-3 whitespace-nowrap">
-          <div className="flex items-center">
-            <div className="w-auto h-auto">{lockedAmount.amount}</div>
-          </div>
-        </td>
-      </tr>
-    </>
+        </div>
+      </div>
+      <div className="px-6 py-3 whitespace-nowrap">
+        <div className="flex items-center">
+          <div className="w-auto h-auto">{lockedAmount.amount}</div>
+        </div>
+      </div>
+    </div>
   )
 }
--- src/components/tokens/LockedTokenRowSkeleton.tsx
+++ src/components/tokens/LockedTokenRowSkeleton.tsx
@@ -1,22 +1,20 @@
 export default function LockedTokenRowSkeleton() {
   return (
-    <>
-      <tr className="table-row cursor-pointer bg-brand-gray animate animate-pulse">
-        <td className="px-6 py-3 whitespace-nowrap">
-          <div className="flex items-center">
-            <div className="w-10 h-auto bg-white rounded">
-              <div className="invisible">123</div>
-            </div>
+    <div className="table-row cursor-pointer bg-brand-gray animate animate-pulse">
+      <div className="px-6 py-3 whitespace-nowrap">
+        <div className="flex items-center">
+          <div className="w-10 h-auto bg-white rounded">
+            <div className="invisible">123</div>
           </div>
-        </td>
-        <td className="px-6 py-3 whitespace-nowrap">
-          <div className="flex items-center">
-            <div className="invisible w-10 h-auto bg-white rounded">
-              <div className="invisible">123</div>
-            </div>
+        </div>
+      </div>
+      <div className="px-6 py-3 whitespace-nowrap">
+        <div className="flex items-center">
+          <div className="invisible w-10 h-auto bg-white rounded">
+            <div className="invisible">123</div>
           </div>
-        </td>
-      </tr>
-    </>
+        </div>
+      </div>
+    </div>
   )
 }
