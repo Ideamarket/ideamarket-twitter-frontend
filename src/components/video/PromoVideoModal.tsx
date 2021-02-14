cd6c27b70490dc81a36644e1c58e095fe078d5dd
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2021-02-14 20:26:02 +0100

    Update promo video link

--- src/components/video/PromoVideoModal.tsx
+++ src/components/video/PromoVideoModal.tsx
@@ -49,7 +49,7 @@ export default function PromoVideoModal({
         <iframe
           width="100%"
           height="100%"
-          src="https://www.youtube.com/embed/rbE53XUtVw0"
+          src="https://www.youtube.com/embed/vmzUtpaeQ-I"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
