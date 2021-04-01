c1b476b72293129615fe6e264bd8ba0783d1c046
Bhanu Teja Pachipulusu <pbteja1998@gmail.com>
2021-04-01 15:38:07 +0530

    Prevent adding the already existing email to airtable

--- src/pages/api/airtable-send.ts
+++ src/pages/api/airtable-send.ts
@@ -6,6 +6,8 @@ const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
 )
 
 const baseName = process.env.BASE_NAME
+const viewName = process.env.VIEW_NAME
+const fieldName = process.env.FIELD_NAME
 
 type Data = {
   message: string
@@ -15,6 +17,19 @@ const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   const { email } = req.body
 
   try {
+    const records = await base(baseName)
+      .select({
+        maxRecords: 1,
+        view: viewName,
+        filterByFormula: `{${fieldName}}="${email}"`,
+      })
+      .firstPage()
+
+    if (records.length > 0 && records[0].get(fieldName) === email) {
+      res.status(200).json({ message: 'Successfully added to our email list!' })
+      return
+    }
+
     await base(baseName).create(
       [
         {
