a6b3c363a4074245a7a48d4777fff2633bae448c
Mateusz Jasiuk <mateusz.jasiuk@gmail.com>
2021-04-02 19:30:42 +0200

    feat: mobile row - skeleton (IDEAMARKET-22)

--- src/components/tokens/OverviewTokenRowSkeleton.tsx
+++ src/components/tokens/OverviewTokenRowSkeleton.tsx
@@ -3,11 +3,11 @@ import classNames from 'classnames'
 export default function TokenRowSkeleton() {
   return (
     <>
-      <tr className="grid grid-cols-3 md:table-row animate animate-pulse">
-        <td className="pl-3 py-4 whitespace-nowrap hidden md:table-cell">
+      <tr className="grid grid-cols-mobile-row grid-flow-col md:table-row animate animate-pulse">
+        <td className="hidden py-4 pl-3 pr-1 md:table-cell">
           <div className="w-8 h-5 bg-gray-400 rounded"></div>
         </td>
-        <td className="col-span-3 px-6 py-4 whitespace-nowrap">
+        <td className="flex md:table-cell md:col-span-3 py-4 pl-2 md:pl-6 whitespace-nowrap">
           <div className="flex items-center">
             <div
               className={classNames(
@@ -15,37 +15,37 @@ export default function TokenRowSkeleton() {
               )}
             ></div>
             <div className="w-20 h-4 ml-4 bg-gray-400 rounded"></div>
-            <div className="flex items-center justify-center ml-auto md:hidden">
-              <div className="w-7.5 h-4 bg-gray-400 rounded"></div>
-            </div>
           </div>
         </td>
-        <td className="px-5 py-4 whitespace-nowrap">
+        <td className="hidden md:table-cell py-4 pl-6 whitespace-nowrap">
+          <p className="h-4 w-8 bg-gray-400 rounded"></p>
+        </td>
+        <td className="hidden md:table-cell py-4 pl-6 whitespace-nowrap">
+          <p className="h-4 w-10 bg-gray-400 rounded"></p>
+        </td>
+        <td className="hidden md:table-cell py-4 pl-6 whitespace-nowrap">
           <p className="w-10 h-4 bg-gray-400 rounded md:hidden"></p>
           <p className="h-4 mt-1 bg-gray-400 rounded"></p>
         </td>
-        <td className="px-5 py-4 whitespace-nowrap">
+        <td className="flex md:table-cell items-center py-4 pl-4 md:pl-6 whitespace-nowrap">
           <p className="w-10 h-4 bg-gray-400 rounded md:hidden"></p>
           <p className="h-4 mt-1 bg-gray-400 rounded"></p>
         </td>
-        <td className="px-5 py-4 whitespace-nowrap">
+        <td className="hidden md:table-cell py-4 pl-6 whitespace-nowrap">
           <p className="w-10 h-4 bg-gray-400 rounded md:hidden"></p>
           <p className="h-4 mt-1 bg-gray-400 rounded"></p>
         </td>
-        <td className="px-5 py-4 whitespace-nowrap">
-          <p className="w-10 h-4 bg-gray-400 rounded md:hidden"></p>
-          <p className="h-4 mt-1 bg-gray-400 rounded"></p>
-        </td>
-        <td className="px-5 py-4 whitespace-nowrap">
-          <p className="w-10 h-4 bg-gray-400 rounded md:hidden"></p>
-          <p className="h-4 mt-1 bg-gray-400 rounded"></p>
-        </td>
-        <td className="hidden px-5 py-4 whitespace-nowrap md:table-cell">
+        <td className="hidden md:table-cell px-6 py-4 whitespace-nowrap">
           <button className="h-10 bg-gray-400 rounded-lg w-24">
             <span className="invisible">Trade</span>
           </button>
         </td>
-        <td className="px-3 py-4 whitespace-nowrap">
+        <td className="md:hidden pl-4 py-4 whitespace-nowrap">
+          <button className="px-2 py-1 rounded-lg bg-gray-400">
+            <span className="invisible">$0.00</span>
+          </button>
+        </td>
+        <td className="py-4 px-3 md:pl-3 md:pr-6 whitespace-nowrap">
           <div className="flex justify-center items-center h-full">
             <div className="w-5 h-5 bg-gray-400 rounded"></div>
           </div>
