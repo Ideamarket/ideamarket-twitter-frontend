e33ecbbe3351e1fb6b0be38142def0cc30aaad9d
Bhanu Teja Pachipulusu <pbteja1998@gmail.com>
2021-04-09 21:22:36 +0530

    Move the mutual holders section a little to the top

--- src/components/mutual-tokens/MutualTokensList.tsx
+++ src/components/mutual-tokens/MutualTokensList.tsx
@@ -121,17 +121,19 @@ export default function MutualTokensList({
         )}
       </dl>
 
-      {pageNumber * PAGE_SIZE < mutualHoldersList.length && (
-        <div className="flex items-center justify-center my-10">
-          <button
-            type="button"
-            className="inline-flex items-center px-6 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            onClick={() => setPageNumber(pageNumber + 1)}
-          >
-            Load more
-          </button>
-        </div>
-      )}
+      {!isLoading &&
+        mutualHoldersList &&
+        pageNumber * PAGE_SIZE < mutualHoldersList.length && (
+          <div className="flex items-center justify-center my-10">
+            <button
+              type="button"
+              className="inline-flex items-center px-6 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              onClick={() => setPageNumber(pageNumber + 1)}
+            >
+              Load more
+            </button>
+          </div>
+        )}
     </>
   )
 }
