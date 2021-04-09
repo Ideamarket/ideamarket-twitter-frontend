4020899e7a75a2fafb3f487b43c8d2977b72650c
Bhanu Teja Pachipulusu <pbteja1998@gmail.com>
2021-04-09 21:30:45 +0530

    Change load more button color

--- src/components/mutual-tokens/MutualTokensList.tsx
+++ src/components/mutual-tokens/MutualTokensList.tsx
@@ -127,7 +127,7 @@ export default function MutualTokensList({
           <div className="flex items-center justify-center my-10">
             <button
               type="button"
-              className="inline-flex items-center px-6 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              className="inline-flex items-center px-6 py-3 text-base font-medium text-white border border-transparent rounded-md shadow-sm bg-very-dark-blue hover:bg-very-dark-blue-3 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               onClick={() => setPageNumber(pageNumber + 1)}
             >
               Load more
