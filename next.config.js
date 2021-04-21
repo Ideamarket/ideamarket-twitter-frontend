5cc216137b25fc795419107f4b2a52cd4ae584f4
Bhanu Teja Pachipulusu <pbteja1998@gmail.com>
2021-04-21 18:38:25 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,9 +8,14 @@ module.exports = {
             type: 'host',
             value: 'attn.to',
           },
+          {
+            type: 'header',
+            key: ':authority',
+            value: 'attn.to'
+          }
         ],
         permanent: true,
-        destination: 'https://app.ideamarket.io/:path*',
+        destination: `https://${process.env.VERCEL_ENV === 'preview' ? process.env.VERCEL_URL : 'app.ideamarket.io'}/:path*`,
       },
     ]
   },
