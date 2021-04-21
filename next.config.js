30787e3b3981f7cfa4ce127ba045efc306170801
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2021-04-21 15:29:57 +0200

    feat: remove check for authority header

--- next.config.js
+++ next.config.js
@@ -8,14 +8,13 @@ module.exports = {
             type: 'host',
             value: 'attn.to',
           },
-          {
-            type: 'header',
-            key: ':authority',
-            value: 'attn.to'
-          }
         ],
         permanent: true,
-        destination: `https://${process.env.VERCEL_ENV === 'preview' ? process.env.VERCEL_URL : 'app.ideamarket.io'}/:path*`,
+        destination: `https://${
+          process.env.VERCEL_ENV === 'preview'
+            ? process.env.VERCEL_URL
+            : 'app.ideamarket.io'
+        }/:path*`,
       },
     ]
   },
