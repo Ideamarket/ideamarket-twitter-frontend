10a50b9aee76949c5795640734e4e100f07b7172
Shmoji <jackj_skate@hotmail.com>
2021-05-05 08:45:59 -0400

    fix(IDEAMARKET-5): do not block site from rendering if wallet not connected

--- src/components/wallet/Web3ReactManager.tsx
+++ src/components/wallet/Web3ReactManager.tsx
@@ -11,10 +11,5 @@ export default function Web3ReactManager({
   // when there's no account connected, react to logins (broadly speaking) on the injected provider, if it exists
   useInactiveListener(!triedEager)
 
-  // on page load, do nothing until we've tried to connect to the injected connector
-  if (!triedEager) {
-    return null
-  }
-
   return children
 }
