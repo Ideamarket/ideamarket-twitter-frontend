4a2e4779caec171bd5a1391c512aa400bcc71d14
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2021-05-06 14:11:14 +0200

    feat: serve user images from cloudfront

--- src/actions/queryLambdavatar.ts
+++ src/actions/queryLambdavatar.ts
@@ -7,14 +7,10 @@ export default async function queryLambdavatar({
 }): Promise<string> {
   return fetch(
     `https://lambdavatar.backend.ideamarket.io/${rawMarketName}/${rawTokenName}`
-  )
-    .then((res) => {
-      if (!res.ok) {
-        return {
-          url: 'https://app.ideamarket.io/logo.png',
-        }
-      }
-      return res.json()
-    })
-    .then((result) => result.url)
+  ).then((res) => {
+    if (!res.ok) {
+      return 'https://app.ideamarket.io/logo.png'
+    }
+    return `https://d3hjr60szea5ud.cloudfront.net/${rawMarketName}/${rawTokenName}.png`
+  })
 }
