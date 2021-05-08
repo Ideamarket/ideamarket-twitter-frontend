f39c45cad6cd94a599ae8f7830a9ed75714b55f2
Bhanu Teja Pachipulusu <pbteja1998@gmail.com>
2021-05-08 19:56:16 +0530

    feat: get svg icon from marketspecifics for the iframe embed

--- src/pages/iframe/[marketName]/[tokenName].tsx
+++ src/pages/iframe/[marketName]/[tokenName].tsx
@@ -1,20 +1,20 @@
 import { useTokenIconURL } from 'actions'
+import { A } from 'components'
 import { useRouter } from 'next/router'
 import { getMarketSpecificsByMarketNameInURLRepresentation } from 'store/markets'
-import SubstackBlack from '../../../assets/substack-black.svg'
 
 export default function IframeEmbed() {
   const router = useRouter()
-  const marketName = router.query.marketName as string
-  const tokenName = router.query.tokenName as string
+  const rawMarketName = router.query.marketName as string
+  const rawTokenName = router.query.tokenName as string
 
   const marketSpecifics = getMarketSpecificsByMarketNameInURLRepresentation(
-    marketName
+    rawMarketName
   )
 
-  const { tokenIconURL, isLoading: isTokenIconURLLoading } = useTokenIconURL({
+  const { tokenIconURL, isLoading } = useTokenIconURL({
     marketSpecifics,
-    tokenName: marketSpecifics?.getTokenNameFromURLRepresentation(tokenName),
+    tokenName: marketSpecifics?.getTokenNameFromURLRepresentation(rawTokenName),
   })
 
   if (!router.isReady) {
@@ -24,27 +24,34 @@ export default function IframeEmbed() {
   return (
     <div className="flex justify-center items-center h-full">
       <div className="flex justify-around items-center w-80 h-14 border bg-white rounded-lg border-black border-opacity-10 p-2">
-        <div className="h-10 w-10 bg-black rounded-md border bg-opacity-10  flex justify-center items-center">
-          <SubstackBlack />
+        <div className="h-10 w-10 p-2.5 bg-black rounded-md border bg-opacity-10  flex justify-center items-center">
+          {marketSpecifics.getMarketOutlineSVG()}
         </div>
         <div className="flex">
           <div className="w-6 h-6 rounded-full overflow-hidden">
-            <img
-              src={tokenIconURL}
-              alt={tokenName}
-              className="object-cover w-full h-full"
-            />
+            {isLoading ? (
+              <div className="w-full h-full bg-gray-400 animate-pulse" />
+            ) : (
+              <img
+                src={tokenIconURL}
+                alt={rawTokenName}
+                className="object-cover w-full h-full"
+              />
+            )}
           </div>
-          <p className="text-[#323232] font-bold ml-1">{tokenName}</p>
+          <p className="text-[#323232] font-bold ml-1">{rawTokenName}</p>
         </div>
         <div className="flex items-center h-full w-[131px] bg-black bg-opacity-10 rounded-md border border-black border-opacity-10">
           <div className="flex-1 flex justify-center items-center h-full text-sm font-medium text-[#373737] text-opacity-100">
             $0.79
           </div>
           <div className="h-full flex-1 rounded-md overflow-hidden">
-            <button className="text-sm font-bold text-white bg-[#1534d9] w-full h-full">
+            <A
+              className="text-sm font-bold text-white bg-[#1534d9] w-full h-full flex justify-center items-center"
+              href={`https://app.ideamarket.io/i/${rawMarketName}/${rawTokenName}`}
+            >
               Buy
-            </button>
+            </A>
           </div>
         </div>
       </div>
