f13dcdcf7aae010224644a28392a05c1aa827247
Shmoji <jackj_skate@hotmail.com>
2021-06-16 11:10:42 -0400

    fix: big number addition fix

--- src/components/trade/TradeInterface.tsx
+++ src/components/trade/TradeInterface.tsx
@@ -173,7 +173,7 @@ export default function TradeInterface({
 
   const ideaTokenValue = web3BNToFloatString(
     calculateIdeaTokenDaiValue(
-      ideaToken?.rawSupply.add(new BN(masterIdeaTokenAmount)),
+      ideaToken?.rawSupply.add(masterIdeaTokenAmountBN),
       market,
       masterIdeaTokenAmountBN
     ),
