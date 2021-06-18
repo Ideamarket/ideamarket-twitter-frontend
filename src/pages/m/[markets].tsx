aae85ca55d44a1042eaadab407dcb7e8b3902b51
Shmoji <jackj_skate@hotmail.com>
2021-06-18 13:50:38 -0400

    fix: redirecting a different way since other way didn't work on deployed site

--- src/pages/m/[markets].tsx
+++ src/pages/m/[markets].tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { useRouter } from 'next/router'
 import { DefaultLayout, Home as HomeComponent } from 'components'
 import { DropdownFilters } from 'components/tokens/OverviewFilters'
-import { GetServerSideProps } from 'next'
 
 export default function MarketsHome() {
   const router = useRouter()
@@ -20,29 +19,12 @@ export default function MarketsHome() {
   if (marketsList.length === DropdownFilters.PLATFORMS.values.length - 1) {
     marketsList.push('All')
   }
-
-  return <HomeComponent urlMarkets={marketsList} />
-}
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const { markets } = context.params
-
-  const marketsList = (markets as string)
-    .split('+')
-    .map((m) => m.charAt(0).toUpperCase() + m.slice(1))
-    .filter((m) => DropdownFilters.PLATFORMS.values.includes(m))
-
-  if (markets.length > 0 && marketsList.length === 0) {
+  if (marketsList.length === 0) {
     // Redirect to regular home page if no valid markets
-    return {
-      redirect: {
-        destination: `/`,
-        permanent: true,
-      },
-    }
+    router.push('/')
   }
 
-  return { props: {} }
+  return <HomeComponent urlMarkets={marketsList} />
 }
 
 MarketsHome.layoutProps = {
