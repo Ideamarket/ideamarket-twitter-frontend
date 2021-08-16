064b23ca2353ebb0d7be6ae7f1642be1fcdd46d1
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2021-08-16 14:00:56 +0200

    fix: owned token row l1 box

--- src/components/tokens/OwnedTokenRow.tsx
+++ src/components/tokens/OwnedTokenRow.tsx
@@ -102,7 +102,7 @@ export default function TokenRow({
                 </div>
               )}
             </div>
-            <div className="ml-4 text-base font-semibold leading-5">
+            <div className="flex ml-4 text-base font-semibold leading-5">
               <A
                 href={`${marketSpecifics.getTokenURL(token.name)}`}
                 className="hover:underline"
