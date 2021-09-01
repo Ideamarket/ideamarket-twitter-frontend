3b752b5d2e9730c15172118d33b18f91858125bd
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2021-09-01 10:50:37 +0200

    fix: redirect to backSoon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -27,7 +27,6 @@ import Web3ReactManager from 'components/wallet/Web3ReactManager'
 import ModalRoot from 'components/modals/ModalRoot'
 import { WrongNetworkOverlay } from 'components'
 import { initUseMarketStore } from 'store/markets'
-import router from 'next/router'
 
 export const GlobalContext = createContext({
   onWalletConnectedCallback: () => {},
@@ -56,8 +55,6 @@ function MyApp({ Component, pageProps }: AppProps) {
       ? localStorage.getItem('IS_EMAIL_BAR_CLOSED') === 'true'
       : false
     setIsEmailFooterActive(!isEmailBarClosed)
-
-    router.push('/backSoon')
   }, [])
 
   useEffect(() => {
