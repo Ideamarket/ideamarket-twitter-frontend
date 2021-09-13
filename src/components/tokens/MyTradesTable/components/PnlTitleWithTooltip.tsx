3aa587bced17112ca23120154bdd7e3df26e3987
Shmoji <jackj_skate@hotmail.com>
2021-09-13 14:23:43 -0400

    fix: can now sort by all columns on MyTrades table

--- src/components/tokens/MyTradesTable/components/PnlTitleWithTooltip.tsx
+++ src/components/tokens/MyTradesTable/components/PnlTitleWithTooltip.tsx
@@ -6,8 +6,7 @@ const PnlTitleWithTooltip = () => (
     <Tooltip className="ml-2">
       <div className="w-32 md:w-64">
         PnL stands for Profit and Loss. PnL shows the difference between the
-        value of your holdings at the time of purchase, and their current
-        value."
+        value of your holdings at the time of purchase, and their current value.
       </div>
     </Tooltip>
   </div>
