192764d00458aaea6fb9ae8a1fe129ec4e0d15da
Tuan Phung <tuan@phung.cz>
2021-10-04 13:45:10 +0200

    fix: profile acc

--- src/components/account/SettingsTab.tsx
+++ src/components/account/SettingsTab.tsx
@@ -14,14 +14,19 @@ const SettingsTab = () => {
       </div>
       <div className="flex flex-col lg:flex-row lg:space-x-2">
         <div className="w-full">
-          <div className="p-3 font-bold">Account Information</div>
+          <div className="flex">
+            <div className="lg:w-44" />
+            <div className="p-3 font-bold">Account Information</div>
+          </div>
           <div className="flex flex-col w-full px-3 py-2 space-y-2 rounded-lg bg-blue-50 dark:bg-gray-600">
-            <div className="flex flex-col justify-between sm:flex-row">
-              <span>Username</span>
+            <div className="flex flex-col items-center justify-end lg:flex-row">
+              <span className="w-full text-left lg:w-40 lg:mr-4 lg:text-right">
+                Username
+              </span>
 
               <input
                 type="text"
-                className="block w-full h-8 border-gray-300 rounded-md shadow-sm sm:w-64 sm:text-sm focus:outline-none dark:text-gray-300 dark:bg-gray-600 dark:placeholder-gray-200 dark:border-gray-500 focus:ring-brand-blue focus:border-brand-blue"
+                className="block w-full h-8 border-gray-300 rounded-md shadow-sm lg:w-60 lg:text-sm focus:outline-none dark:text-gray-300 dark:bg-gray-600 dark:placeholder-gray-200 dark:border-gray-500 focus:ring-brand-blue focus:border-brand-blue"
                 {...register('username')}
                 onChange={(e) => {
                   if (!username) {
@@ -31,58 +36,65 @@ const SettingsTab = () => {
                 disabled={!!username}
               />
             </div>
-            <div className="flex flex-col justify-between sm:flex-row">
-              <span>Email Address</span>
+            <div className="flex flex-col items-center justify-end lg:flex-row">
+              <span className="w-full text-left lg:w-40 lg:mr-4 lg:text-right">
+                Email Address
+              </span>
               <input
                 type="email"
-                className="block w-full h-8 border-gray-300 rounded-md shadow-sm sm:w-64 sm:text-sm focus:outline-none dark:text-gray-300 dark:bg-gray-600 dark:placeholder-gray-200 dark:border-gray-500 focus:ring-brand-blue focus:border-brand-blue"
+                className="block w-full h-8 border-gray-300 rounded-md shadow-sm lg:w-60 lg:text-sm focus:outline-none dark:text-gray-300 dark:bg-gray-600 dark:placeholder-gray-200 dark:border-gray-500 focus:ring-brand-blue focus:border-brand-blue"
                 placeholder="you@example.com"
                 {...register('email')}
                 disabled
               />
             </div>
 
-            {/* <div className="flex flex-col justify-between sm:flex-row">
+            {/* <div className="flex flex-col items-center justify-end lg:flex-row">
               <span>Eth address</span>
               <input
                 type="text"
-                className="block w-full h-8 border-gray-300 rounded-md shadow-sm sm:w-64 sm:text-sm focus:outline-none dark:text-gray-300 dark:bg-gray-600 dark:placeholder-gray-200 dark:border-gray-500 focus:ring-brand-blue focus:border-brand-blue"
+                className="block w-full h-8 border-gray-300 rounded-md shadow-sm lg:w-60 lg:text-sm focus:outline-none dark:text-gray-300 dark:bg-gray-600 dark:placeholder-gray-200 dark:border-gray-500 focus:ring-brand-blue focus:border-brand-blue"
                 {...register('ethAddresses')}
                 disabled={isUpdateLoading}
               />
             </div> */}
 
-            <div className="flex flex-col justify-between sm:flex-row">
-              <span>Redirection url</span>
+            <div className="flex flex-col items-center justify-end lg:flex-row">
+              <span className="w-full text-left lg:w-40 lg:mr-4 lg:text-right">
+                attn.to/
+              </span>
               <input
                 type="text"
-                className="block w-full h-8 border-gray-300 rounded-md shadow-sm sm:w-64 sm:text-sm focus:outline-none dark:text-gray-300 dark:bg-gray-600 dark:placeholder-gray-200 dark:border-gray-500 focus:ring-brand-blue focus:border-brand-blue"
+                className="block w-full h-8 border-gray-300 rounded-md shadow-sm lg:w-60 lg:text-sm focus:outline-none dark:text-gray-300 dark:bg-gray-600 dark:placeholder-gray-200 dark:border-gray-500 focus:ring-brand-blue focus:border-brand-blue"
                 {...register('redirectionUrl')}
                 disabled={isUpdateLoading}
               />
             </div>
 
-            <div className="flex flex-col justify-between sm:flex-row">
-              <span>Bio</span>
+            <div className="flex flex-col items-center justify-end lg:flex-row">
+              <span className="w-full text-left lg:w-40 lg:mr-4 lg:text-right">
+                Bio
+              </span>
               <textarea
                 placeholder="Bio"
                 {...register('bio')}
                 disabled={isUpdateLoading}
-                className="w-full border-gray-300 rounded-md sm:w-64 dark:border-gray-500 dark:bg-gray-600"
+                className="w-full border-gray-300 rounded-md lg:w-60 dark:border-gray-500 dark:bg-gray-600"
               />
             </div>
 
-            <div className="flex-col space-y-2">
-              <div className="flex space-x-2">
-                <label>Upload Profile Photo</label>
-                <input
-                  {...register('imageFile')}
-                  onChange={async (e) => setValue('imageFile', e.target.files)}
-                  type="file"
-                  accept="image/png, image/jpeg"
-                  disabled={isUpdateLoading}
-                />
-              </div>
+            <div className="flex flex-col items-center justify-end lg:flex-row">
+              <span className="w-full text-left lg:w-40 lg:mr-4 lg:text-right">
+                Profile Photo
+              </span>
+              <input
+                {...register('imageFile')}
+                onChange={async (e) => setValue('imageFile', e.target.files)}
+                type="file"
+                accept="image/png, image/jpeg"
+                disabled={isUpdateLoading}
+                className="w-full lg:w-60"
+              />
             </div>
           </div>
         </div>
