Ideamarket/ideamarket-frontend

feat: connected merkleDistributor smart contracts to frontend for rinkeby

src/actions/useBalance.ts
@@ -6,9 +6,10 @@ import BigNumber from 'bignumber.js'
 import BN from 'bn.js'
 
 export default function useBalance(
-  address: string,
+  tokenAddress: string,
+  userAddress: string,
   decimals: number,
-  tradeToggle: boolean
+  tradeToggle: any
 ) {
   const [isLoading, setIsLoading] = useState(true)
   const [balanceBN, setBalanceBN] = useState(undefined)
@@ -21,33 +22,37 @@ export default function useBalance(
 
     function getBalance() {
       return new Promise<BN>((resolve) => {
-        if (!web3 || !address) {
+        if (!web3 || !tokenAddress) {
           resolve(new BN('0'))
           return
         }
 
-        if (address === ZERO_ADDRESS) {
-          web3.eth
-            .getBalance(useWalletStore.getState().address)
-            .then((value) => {
-              resolve(new BN(value))
-            })
-            .catch((error) => {
-              console.log(error)
-              resolve(new BN('0'))
-            })
-        } else {
-          const contract = getERC20Contract(address)
-          contract.methods
-            .balanceOf(useWalletStore.getState().address)
-            .call()
-            .then((value) => {
-              resolve(new BN(value))
-            })
-            .catch((error) => {
-              console.log(error)
-              resolve(new BN('0'))
-            })
+        try {
+          if (tokenAddress === ZERO_ADDRESS) {
+            web3.eth
+              .getBalance(userAddress)
+              .then((value) => {
+                resolve(new BN(value))
+              })
+              .catch((error) => {
+                console.log(error)
+                resolve(new BN('0'))
+              })
+          } else {
+            const contract = getERC20Contract(tokenAddress)
+            contract.methods
+              .balanceOf(userAddress)
+              .call()
+              .then((value) => {
+                resolve(new BN(value))
+              })
+              .catch((error) => {
+                console.log(error)
+                resolve(new BN('0'))
+              })
+          }
+        } catch (error) {
+          resolve(new BN('0'))
         }
       })
     }
@@ -68,7 +73,7 @@ export default function useBalance(
     return () => {
       isCancelled = true
     }
-  }, [address, web3, tradeToggle, decimals])
+  }, [tokenAddress, web3, tradeToggle, decimals, userAddress])
 
-  return [isLoading, balanceBN, balance]
+  return [balance, balanceBN, isLoading]
 }

Ideamarket/ideamarket-twitter-frontend

src/actions/useBalance.ts
@@ -6,7 +6,7 @@ import BigNumber from 'bignumber.js'
 import BN from 'bn.js'
 
 export default function useBalance(
-  address: string,
+  tokenAddress: string, // This can be token or contract address
   decimals: number,
   refreshToggle?: boolean // Used to refresh balance whenever needed
 ) {
@@ -25,33 +25,37 @@ export default function useBalance(
 
     function getBalance() {
       return new Promise<BN>((resolve) => {
-        if (!web3 || !address) {
+        if (!web3 || !tokenAddress) {
           resolve(new BN('0'))
           return
         }
 
-        if (address === ZERO_ADDRESS) {
-          web3.eth
-            .getBalance(useWalletStore.getState().address)
-            .then((value) => {
-              resolve(new BN(value))
-            })
-            .catch((error) => {
-              console.error('Getting balance of ETH failed', error)
-              resolve(new BN('0'))
-            })
-        } else {
-          const contract = getERC20Contract(address)
-          contract.methods
-            .balanceOf(useWalletStore.getState().address)
-            .call()
-            .then((value) => {
-              resolve(new BN(value))
-            })
-            .catch((error) => {
-              console.error('Getting balance of ERC20 failed', error)
-              resolve(new BN('0'))
-            })
+        try {
+          if (tokenAddress === ZERO_ADDRESS) {
+            web3.eth
+              .getBalance(walletAddress)
+              .then((value) => {
+                resolve(new BN(value))
+              })
+              .catch((error) => {
+                console.error('Getting balance of ETH failed', error)
+                resolve(new BN('0'))
+              })
+          } else {
+            const contract = getERC20Contract(tokenAddress)
+            contract.methods
+              .balanceOf(walletAddress)
+              .call()
+              .then((value) => {
+                resolve(new BN(value))
+              })
+              .catch((error) => {
+                console.error('Getting balance of ERC20 failed', error)
+                resolve(new BN('0'))
+              })
+          }
+        } catch (error) {
+          resolve(new BN('0'))
         }
       })
     }
@@ -72,7 +76,7 @@ export default function useBalance(
     return () => {
       isCancelled = true
     }
-  }, [address, web3, walletAddress, refreshToggle, decimals])
+  }, [tokenAddress, web3, walletAddress, refreshToggle, decimals])
 
-  return [isLoading, balanceBN, balance]
+  return [balance, balanceBN, isLoading]
 }