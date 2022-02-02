2aa5cf78895edd9141a922f1e882e9ebc58695e1
sergeim28 <sergiymisyura28@gmail.com>
2022-02-02 22:48:28 +0200

    fix: use proper merkleroot in claimimo call

--- src/actions/claimIMO.tsx
+++ src/actions/claimIMO.tsx
@@ -1,8 +1,10 @@
-import MerkleRoot from 'assets/merkle-root.json'
+import UserMerkleRoot from 'assets/merkle-root.json'
+import CommunityMerkleRoot from 'assets/community-merkle-root.json'
 import { useContractStore } from 'store/contractStore'
 import { isAddressInMerkleRoot } from 'utils/merkleRoot'
 
 export default function claimIMO(address: string, isCommunityAirdrop: boolean) {
+  const MerkleRoot = isCommunityAirdrop ? CommunityMerkleRoot : UserMerkleRoot
   if (isAddressInMerkleRoot(address, isCommunityAirdrop)) {
     const claim = MerkleRoot.claims[address]
 
