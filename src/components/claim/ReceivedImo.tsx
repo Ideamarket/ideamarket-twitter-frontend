afdc201f8684fff37c40e4f51c6e680a17639f22
sergeim28 <sergiymisyura28@gmail.com>
2022-02-09 22:00:21 +0200

    chore: change apy numbers of claim received page

--- src/components/claim/ReceivedImo.tsx
+++ src/components/claim/ReceivedImo.tsx
@@ -129,7 +129,7 @@ const ReceivedImo: React.FC<Props> = ({ airdropType }) => {
                   Listing IMO Staking
                 </span>
                 <span className="text-brand-neon-green text-2xl font-extrabold font-inter">
-                  13000% APY
+                  550% APY
                 </span>
               </div>
               <div className="flex flex-col border-white/20 border text-left p-1 py-5 justify-center bg-white/5 rounded-xl  text-center">
@@ -137,7 +137,7 @@ const ReceivedImo: React.FC<Props> = ({ airdropType }) => {
                   Stake IMO
                 </span>
                 <span className="text-brand-neon-green text-2xl font-extrabold font-inter">
-                  200% APY
+                  300% APY
                 </span>
               </div>
             </div>
