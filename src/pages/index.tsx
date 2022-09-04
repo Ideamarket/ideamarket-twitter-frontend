7e1ba99d57651762a7f80ff1af07834bb914fafc
Shmoji <jackj_skate@hotmail.com>
2022-09-03 23:44:16 -0400

    chore: default time filter 1week

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -64,7 +64,7 @@ const Home = () => {
   const [selectedCategories, setSelectedCategories] = useState([])
   const [selectedView, setSelectedView] = useState(HOME_PAGE_VIEWS.POSTS)
   const [isAdvancedView, setIsAdvancedView] = useState(true)
-  const [timeFilter, setTimeFilter] = useState(TIME_FILTER.ONE_DAY)
+  const [timeFilter, setTimeFilter] = useState(TIME_FILTER.ONE_WEEK)
 
   const [isTimeFilterDropdownOpen, setIsTimeFilterDropdownOpen] =
     useState(false)
