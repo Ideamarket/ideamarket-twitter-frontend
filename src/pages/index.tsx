3214a258e1710c96f56ea60da5e89f329900b60c
Joshua Jackson <skate.jackj@gmail.com>
2022-09-14 15:24:58 -0400

    chore: all time home filter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -64,7 +64,7 @@ const Home = () => {
   const [selectedCategories, setSelectedCategories] = useState([])
   const [selectedView, setSelectedView] = useState(HOME_PAGE_VIEWS.POSTS)
   const [isAdvancedView, setIsAdvancedView] = useState(true)
-  const [timeFilter, setTimeFilter] = useState(TIME_FILTER.ONE_WEEK)
+  const [timeFilter, setTimeFilter] = useState(TIME_FILTER.ALL_TIME)
 
   const [isTimeFilterDropdownOpen, setIsTimeFilterDropdownOpen] =
     useState(false)
