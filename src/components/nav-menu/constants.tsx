058236b073b93850116689085af81d83fca3167e
Shmoji <jackj_skate@hotmail.com>
2022-11-21 16:06:38 -0500

    chore: imo header link back

--- src/components/nav-menu/constants.tsx
+++ src/components/nav-menu/constants.tsx
@@ -1,5 +1,5 @@
-// import router from 'next/router'
-// import { NETWORK } from 'store/networks'
+import router from 'next/router'
+import { NETWORK } from 'store/networks'
 
 export const getNavbarConfig = (user: any) => ({
   menu: [
@@ -67,61 +67,61 @@ export const getNavbarConfig = (user: any) => ({
         },
       ],
     },
-    // {
-    //   name: '$IMO',
-    //   subMenu: [
-    //     {
-    //       id: 1,
-    //       name: <b>Buy IMO on SushiSwap</b>,
-    //       onClick: () => {
-    //         window.open(
-    //           'https://app.sushi.com/swap?inputCurrency=ETH&outputCurrency=0xB41bd4C99dA73510d9e081C5FADBE7A27Ac1F814',
-    //           '_blank'
-    //         )
-    //       },
-    //     },
-    //     {
-    //       id: 2,
-    //       name: 'Add $IMO to Metamask',
-    //       onClick: async () => {
-    //         try {
-    //           const { ethereum } = window as any
-    //           await ethereum.request({
-    //             method: 'wallet_watchAsset',
-    //             params: {
-    //               type: 'ERC20',
-    //               options: {
-    //                 address: NETWORK.getExternalAddresses().imo,
-    //                 symbol: `IMO`,
-    //                 decimals: 18,
-    //                 image: 'https://ideamarket.io/imo-logo.png',
-    //               },
-    //             },
-    //           })
-    //         } catch (ex) {
-    //           // We don't handle that error for now
-    //           // Might be a different wallet than Metmask
-    //           // or user declined
-    //           console.log(ex)
-    //         }
-    //       },
-    //     },
-    //     {
-    //       id: 3,
-    //       name: 'Token Address',
-    //       onClick: () => {
-    //         window.open(
-    //           'https://arbiscan.io/address/0xB41bd4C99dA73510d9e081C5FADBE7A27Ac1F814',
-    //           '_blank'
-    //         )
-    //       },
-    //     },
-    //     {
-    //       id: 4,
-    //       name: 'Unstake from old version',
-    //       onClick: () => router.push('/stake'),
-    //     },
-    //   ],
-    // },
+    {
+      name: '$IMO',
+      subMenu: [
+        {
+          id: 1,
+          name: <b>Buy IMO on SushiSwap</b>,
+          onClick: () => {
+            window.open(
+              'https://app.sushi.com/swap?inputCurrency=ETH&outputCurrency=0xB41bd4C99dA73510d9e081C5FADBE7A27Ac1F814',
+              '_blank'
+            )
+          },
+        },
+        {
+          id: 2,
+          name: 'Add $IMO to Metamask',
+          onClick: async () => {
+            try {
+              const { ethereum } = window as any
+              await ethereum.request({
+                method: 'wallet_watchAsset',
+                params: {
+                  type: 'ERC20',
+                  options: {
+                    address: NETWORK.getExternalAddresses().imo,
+                    symbol: `IMO`,
+                    decimals: 18,
+                    image: 'https://ideamarket.io/imo-logo.png',
+                  },
+                },
+              })
+            } catch (ex) {
+              // We don't handle that error for now
+              // Might be a different wallet than Metmask
+              // or user declined
+              console.log(ex)
+            }
+          },
+        },
+        {
+          id: 3,
+          name: 'Token Address',
+          onClick: () => {
+            window.open(
+              'https://arbiscan.io/address/0xB41bd4C99dA73510d9e081C5FADBE7A27Ac1F814',
+              '_blank'
+            )
+          },
+        },
+        {
+          id: 4,
+          name: 'Unstake from old version',
+          onClick: () => router.push('/stake'),
+        },
+      ],
+    },
   ],
 })
